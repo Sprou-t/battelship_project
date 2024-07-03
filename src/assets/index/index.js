65dc8b00ad71dcec656062b0799415247430b092
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-07-03 23:30:52 +0800

    added ship items to UI

--- src/assets/index/index.js
+++ src/assets/index/index.js
@@ -3,7 +3,12 @@ import './index.css';
 import './reset.css';
 import gameBoard from './gameboard/gameboard';
 import createShip from './ship/ship';
-import { addBoard1Grids, addBoard2Grids, clickGrid, getGridCoordinate } from './boardGrid/grid';
+import {
+	addBoard1Grids,
+	addBoard2Grids,
+	clickGrid,
+	getGridCoordinate,
+} from './boardGrid/grid';
 
 const createPage = (function () {
 	const body = document.querySelector('body');
@@ -13,7 +18,33 @@ const createPage = (function () {
 	const boardDiv = document.createElement('div');
 	boardDiv.classList.add('boardDiv');
 
-	// create 2 boards & 1 text line
+	// create 2 boards, 1 ship div &  1 text line
+	const shipDiv = document.createElement('div');
+	shipDiv.classList.add('shipDiv');
+
+	const carrierShip = document.createElement('p');
+	carrierShip.textContent = 'Carrier';
+	carrierShip.classList.add('Carrier', 'ship');
+
+	const battleship = document.createElement('p');
+	battleship.textContent = 'Battleship';
+	battleship.classList.add('battleship', 'ship');
+
+	const destroyer = document.createElement('p');
+	destroyer.textContent = 'Destroyer';
+	destroyer.classList.add('destroyer', 'ship');
+
+	const submarine = document.createElement('p');
+	submarine.textContent = 'Submarine';
+	submarine.classList.add('submarine', 'ship');
+
+	const patrolBoat = document.createElement('p');
+	patrolBoat.textContent = 'Patrol Boat';
+	patrolBoat.classList.add('patrolBoat', 'ship');
+
+	shipDiv.append(carrierShip, battleship, destroyer, submarine, patrolBoat);
+
+
 	const player1Board = document.createElement('div');
 	player1Board.classList.add('player1Board');
 	const player2Board = document.createElement('div');
@@ -25,14 +56,14 @@ const createPage = (function () {
 	addBoard2Grids(player2Board);
 
 	header.append(textIndicator);
-	boardDiv.append(player1Board, player2Board);
+	boardDiv.append(shipDiv, player1Board, player2Board);
 	body.append(header, boardDiv);
 })();
 
 const gameController = (function () {
-	// TODO:
+	// TODO
+	// add additional class for clicked ships for styling
 	// use coordinate for ship placement
 	// create a button to orientate the ship in another mod
 	// use the grid coordinate on the gameboard class to see if attack hit or missed
-	
 })();
